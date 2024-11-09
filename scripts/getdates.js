fa1f66f58a0a62c411fe9af7af76ab6d04df33b7
Ericevijay12 <ericevijayohiani@gmail.com>
2024-11-09 04:34:25 +0100

    Create getdates.js

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const lastModifiedElement = document.getElementById("lastModified");
+    const lastModifiedDate = new Date(document.lastModified);
+    lastModifiedElement.textContent = lastModifiedDate.toLocaleString();
+});
